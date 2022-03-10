83f5243400e7e8bc33f18eb8ac3c65b530499cea
gelarpambudi <gelargelur@gmail.com>
2022-03-10 22:07:00 +0700

    add csv folder config

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ MODEL_PATH = './model/VGG16-0.3368-0.8985.h5'
 
 app = Flask(__name__)
 app.config["IMG_FOLDER"] = IMG_FOLDER
+app.config["CSV_FOLDER"] = CSV_FOLDER
 app.config["API_KEY"] = os.environ['GOOGLE_STREETVIEW_KEY']
 app.config["MODEL_PATH"] = MODEL_PATH
 app.secret_key = os.environ['APP_SECRET_KEY']
