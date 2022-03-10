88cbc133b5c66223b19c4f41524d47b625adacf9
gelarpambudi <gelargelur@gmail.com>
2022-03-10 22:39:37 +0700

    change raw requests

--- predict.py
+++ predict.py
@@ -38,7 +38,7 @@ def get_image_google(query_params, save_dir=None, csv=False):
             f.write(response.content)
         return save_path
     else:
-        response = requests.get(url=requests_url, params=query_params, stream=True)
+        response = requests.get(url=requests_url, params=query_params, stream=True).raw
         image_arr = np.asarray(bytearray(response.read()), dtype=np.uint8)
         return image_arr
 
@@ -59,7 +59,7 @@ def get_image_osc(query_params, save_dir=None, csv=False):
 
             return save_path
         elif csv == True:
-            image_req = requests.get(url=image_url, stream=True)
+            image_req = requests.get(url=image_url, stream=True).raw
             image_arr = np.asarray(bytearray(image_req.read()), dtype=np.uint8)
             return image_arr
 
